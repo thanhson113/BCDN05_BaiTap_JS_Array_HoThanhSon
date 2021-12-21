bae257f63880276b79938788fd59ea814171f601
Son <sonho1230@gmail.com>
2021-12-22 00:46:05 +0700

    sua bai 7

--- js/main.js
+++ js/main.js
@@ -197,7 +197,7 @@ document.getElementById('btnDoiViTri').onclick = doiViTri;
  */
 function sapXepTang(){
     for(var i = 0; i < numArray.length; i++){
-        for(var j = 0; j < numArray.length; j++){
+        for(var j = 0; j < numArray.length - 1; j++){
             if(numArray[j] > numArray[j + 1]){
                 var temp = numArray[j];
                 numArray[j] = numArray[j + 1];
